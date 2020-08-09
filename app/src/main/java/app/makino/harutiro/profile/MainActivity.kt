8d694adea35f12174777e8751d146cdf2cc3b96b
lifeistech <harutiro2027@outlook.jp>
2020-08-09 17:04:47 +0900

    profile　ver.0.5 完成

--- app/src/main/java/app/makino/harutiro/profile/MainActivity.kt
+++ app/src/main/java/app/makino/harutiro/profile/MainActivity.kt
@@ -2,7 +2,6 @@ package app.makino.harutiro.profile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,13 +9,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-        profileImage.isVisible = false
-        profileCommentText.isVisible = false
-
         button1.setOnClickListener {
-            profileImage.isVisible = true
-            profileCommentText.isVisible = true
+            profileImage.setImageResource(R.drawable.harutiro)
+            profileLabelText.text = "harutiro"
+            profileCommentText.text = "こんにちは、初めてのテキスト入力です！ \n\n~プロフィール~ \n名前：牧野遥斗　\n性別：男性　\n出身地：愛知県　豊川市　\n出身小学校：水戸南部小学校　\n出身中学校：御津中学校　\n現在：豊川工業情報科　         \nその他：将来便利なプログラムを自分で作れるようになりたい！！"
+
+        }
+
+        button2.setOnClickListener {
+            profileImage.setImageResource(R.drawable.urara)
+            profileLabelText.text = "うらら迷路帳"
+            profileCommentText.text = ""
+        }
+
+        button3.setOnClickListener {
+            profileImage.setImageResource(R.drawable.cpu)
+            profileLabelText.text = "used CPU"
+            profileCommentText.text = ""
+        }
+
+        button4.setOnClickListener {
+            profileImage.setImageResource(R.drawable.end)
+            profileLabelText.text = "end"
+            profileCommentText.text = ""
         }
 
 
