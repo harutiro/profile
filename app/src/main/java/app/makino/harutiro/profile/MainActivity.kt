439fef5ede9d5ae3e385de658711866ebbc0b28d
lifeistech <harutiro2027@outlook.jp>
2020-08-09 15:32:09 +0900

    profile　ver.0.1 完成

---
+++ app/src/main/java/app/makino/harutiro/profile/MainActivity.kt
@@ -0,0 +1,26 @@
+package app.makino.harutiro.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.view.isVisible
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        profileImage.isVisible = false
+        profileCommentText.isVisible = false
+
+        button1.setOnClickListener {
+            profileImage.isVisible = true
+            profileCommentText.isVisible = true
+        }
+
+
+
+
+    }
+}
